dd9845d4eec6605924d1614f9b7471043c2b82ea
evilaaron11 <aaron.m.a@comcast.net>
2017-06-01 08:38:30 -0700

    Calibration not fully working

--- main.py
+++ main.py
@@ -5,17 +5,19 @@ import cv2
 import os
 
 IMAGE = "image.jpg"
-PATH = os.cwd()
+PATH = os.getcwd()
 
 def main():
-	os.system('fswebcam ' + PATH)
+	os.system('fswebcam -r 128x720 --no-banner ' + PATH + "/image.jpg")
 	#Read image
 	image = cv2.imread(IMAGE)
 	qty, label, centers, width = seg.identify_target(seg.segment_img(image))
-	calibrate = input("Need to calibrate?: ")
+	calibrate = str(raw_input("Need to calibrate?: "))
 	
 	if calibrate == "y":
 		knownDistance = int(input("How far is the object?: "))
 		for item in width:		
 			print(dist.calibrate(knownDistance, item))
  
+if __name__ == '__main__':
+    main()
