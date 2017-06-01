6be0611c93bc77c048c345e4dcf125d83b0bddfa
evilaaron11 <aaron.m.a@comcast.net>
2017-05-31 19:15:52 -0700

    Added calibration code

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import servo_drivers as driver
 import segment as seg
+import calc_dist as dist
 import cv2
 import os
 
@@ -11,4 +12,10 @@ def main():
 	#Read image
 	image = cv2.imread(IMAGE)
 	qty, label, centers, width = seg.identify_target(seg.segment_img(image))
-
+	calibrate = input("Need to calibrate?: ")
+	
+	if calibrate == "y":
+		knownDistance = int(input("How far is the object?: "))
+		for item in width:		
+			print(dist.calibrate(knownDistance, item))
+ 
